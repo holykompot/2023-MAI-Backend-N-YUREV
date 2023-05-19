f769d25c469146d939b4f04a3c8dc220d895c27f
holykompot <nickj009@yandex.ru>
2023-05-19 15:15:15 +0300

    LRU_v1

--- main.py
+++ main.py
@@ -1,16 +1,69 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from collections import OrderedDict
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class LRUCache:
+
+    # initialising capacity
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()
+        self.capacity = capacity
+
+    # we return the value of the key
+    # that is queried in O(1) and return -1 if we
+    # don't find the key in out dict / cache.
+    # And also move the key to the end
+    # to show that it was recently used.
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        else:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+
+    # first, we add / update the key by conventional methods.
+    # And also move the key to the end to show that it was recently used.
+    # But here we will also check whether the length of our
+    # ordered dictionary has exceeded our capacity,
+    # If so we remove the first key (least recently used)
+    def put(self, key: int, value: int) -> None:
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
+    
+    def _remove(self, node):
+        p = node.prev
+        n = node.next
+        p.next = n
+        n.prev = p
+    def rem(self, key):
+        if key in self.dict:
+            node = self.dict[key]
+            self._remove(node)
+            del self.dict[key]
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# RUNNER
+# initializing our cache with the capacity of 2
+cache = LRUCache(2)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+cache.put(1, 1)
+print(cache.cache)
+cache.put(2, 2)
+print(cache.cache)
+cache.get(1)
+print(cache.cache)
+cache.put(3, 3)
+print(cache.cache)
+cache.get(2)
+print(cache.cache)
+cache.put(4, 4)
+print(cache.cache)
+cache.get(1)
+print(cache.cache)
+cache.get(3)
+print(cache.cache)
+cache.get(4)
+print(cache.cache)
+
+# This code was contributed by Sachin Negi
